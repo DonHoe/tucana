3969296d857b6a8593d40e02449998f3459244b6
hd <hddg@gmail.com>
2018-06-08 16:31:20 +0800

    modify log

--- tucana-web/src/main/java/com/hepic/tucana/web/controller/TestController.java
+++ tucana-web/src/main/java/com/hepic/tucana/web/controller/TestController.java
@@ -2,10 +2,11 @@ package com.hepic.tucana.web.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.hepic.tucana.service.TestService;
-import com.hepic.tucana.util.LoggerUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -15,13 +16,14 @@ public class TestController {
 
     @RequestMapping("/get")
     public String get() {
-        LoggerUtil.warn("debug");
-        LoggerUtil.info("info");
+        log.warn("debug");
+        log.info("info");
         return testService.findUserById(1).getUserName();
     }
 
     @RequestMapping("/getAnswer")
     public String getAnswer(String name) {
+        log.info("info", name);
         String result = JSON.toJSONString(testService.findAnswerByName(name));
         return result;
     }
