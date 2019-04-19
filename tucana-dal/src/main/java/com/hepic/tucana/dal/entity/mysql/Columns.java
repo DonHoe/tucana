581f032b0c5e002536ba425fbbf50e3761246397
HoeDon <houdong0524@live.com>
2019-04-19 17:05:36 +0800

    add table info dao

---
+++ tucana-dal/src/main/java/com/hepic/tucana/dal/entity/mysql/Columns.java
@@ -0,0 +1,74 @@
+package com.hepic.tucana.dal.entity.mysql;
+
+public class Columns {
+
+    private String columnName;
+
+    private Integer ordinalPosition;
+
+    private String columnDefault;
+
+    private String isNullable;
+
+    private String columnType;
+
+    private String columnKey;
+
+    private String extra;
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+
+    public Integer getOrdinalPosition() {
+        return ordinalPosition;
+    }
+
+    public void setOrdinalPosition(Integer ordinalPosition) {
+        this.ordinalPosition = ordinalPosition;
+    }
+
+    public String getColumnDefault() {
+        return columnDefault;
+    }
+
+    public void setColumnDefault(String columnDefault) {
+        this.columnDefault = columnDefault;
+    }
+
+    public String getIsNullable() {
+        return isNullable;
+    }
+
+    public void setIsNullable(String isNullable) {
+        this.isNullable = isNullable;
+    }
+
+    public String getColumnType() {
+        return columnType;
+    }
+
+    public void setColumnType(String columnType) {
+        this.columnType = columnType;
+    }
+
+    public String getColumnKey() {
+        return columnKey;
+    }
+
+    public void setColumnKey(String columnKey) {
+        this.columnKey = columnKey;
+    }
+
+    public String getExtra() {
+        return extra;
+    }
+
+    public void setExtra(String extra) {
+        this.extra = extra;
+    }
+}
