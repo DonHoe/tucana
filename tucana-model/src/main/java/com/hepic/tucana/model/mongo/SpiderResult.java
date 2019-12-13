1997772a7e8414bbe41adde193616870ed7d4813
labtop <hd@email.com>
2019-12-13 13:55:30 +0800

    remove mongodb

--- tucana-model/src/main/java/com/hepic/tucana/model/mongo/SpiderResult.java
+++
@@ -1,9 +0,0 @@
-package com.hepic.tucana.model.mongo;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.HashMap;
-
-@Document(collection = "spider_result")
-public class SpiderResult extends HashMap<String, Object> {
-}
